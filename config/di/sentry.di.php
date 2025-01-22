c5c60fded02dfd799bdab9476a373e31ccf572be
Mariusz Milejko <mariusz@milejko.pl>
2025-01-22 14:28:41 +0100

    fix di definition

--- config/di/sentry.di.php
+++ config/di/sentry.di.php
@@ -10,10 +10,10 @@ use function DI\env;
 return [
     SentryConfig::class => create(SentryConfig::class)
         ->constructor(
-            enabled: env('SENTRY_ENABLED', 1),
+            enabled: env('SENTRY_ENABLED', '0'),
             dsn: env('SENTRY_DSN', ''),
             environment: env('SENTRY_ENVIRONMENT', 'LOCAL'),
-            sampleRate: env('SENTRY_SAMPLE_RATE', 1.0),
+            sampleRate: env('SENTRY_SAMPLE_RATE', '1.0'),
             ignoredExceptions: env('SENTRY_IGNORED_EXCEPTIONS', ''),
             ignoreCode: env('SENTRY_IGNORE_CODE', ''),
             release: env('SENTRY_RELEASE', ''),
