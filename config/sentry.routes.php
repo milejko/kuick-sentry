68a880dbaf6e6b21651e8f5febf11ece7b72a1aa
Mariusz Milejko <mariusz@milejko.pl>
2025-01-29 14:30:30 +0100

    ops unified route

--- config/sentry.routes.php
+++ config/sentry.routes.php
@@ -9,13 +9,15 @@
  */
 
 use Kuick\Framework\Config\RouteConfig;
+use Kuick\Http\Message\RequestInterface;
 
 return [
     // sentry integration testing route
     new RouteConfig(
-        '/api/sentry/exception',
+        '/api/ops/sentry/exception',
         function (): void {
-            throw new RuntimeException('Test exception');
+            throw new RuntimeException('Test exception for sentry integration');
         },
+        [RequestInterface::METHOD_PUT]
     ),
 ];
